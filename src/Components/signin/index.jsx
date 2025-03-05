cfcc6c9d8760585c8c7110a7d9f263fd956e56fa
Mostafa Kadry <mostafakadry806@gmail.com>
2025-03-05 21:11:10 +0200

    Update index.jsx

--- src/Components/signin/index.jsx
+++ src/Components/signin/index.jsx
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { Link, useNavigate  } from "react-router-dom";
+import React, { useState, useEffect, useCallback } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import img from "../../assets/images/review/google.png";
 import img2 from "../../assets/images/review/tweet.png";
 import axios from 'axios';
-import { useDispatch } from 'react-redux'; 
-import { setUser  } from '../../redux/actions/userActions'; 
+import { useDispatch } from 'react-redux';
+import { setUser } from '../../redux/actions/userActions';
 import { FaLinkedin } from 'react-icons/fa';
-// import "./style.css";
+
 function SignIn() {
   const [showPass, setShowPass] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
-  const dispatch = useDispatch(); 
-  const navigate = useNavigate(); 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
-    if(!email || !password) {
+    if (!email || !password) {
       return setError('Error logging in');
     }
     e.preventDefault();
@@ -25,58 +26,57 @@ function SignIn() {
       // Save token to localStorage 
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('email', email);
-      dispatch(setUser(res.data.token)); 
+      dispatch(setUser(res.data.token));
       alert('Logged in successfully');
       navigate('/');
     } catch (err) {
       setError(err.response.data.msg || 'Error logging in');
     }
-  }; 
-  const LinkedInLogin = async () => {
-    window.location.href = `${process.env.REACT_APP_BACKEND_SERVER_URI}/api/auth/linkedin`;  
   };
- 
-  useEffect(() => {
-    const handleLinkedInResponse = async () => {
-      const params = new URLSearchParams(window.location.search);
-      const token = params.get('token');
-     
-      if (token) {
-        // Save token to localStorage
-        localStorage.setItem('token', token);
 
-        
-        try { 
-          const res = await axios.get(`${process.env.REACT_APP_BACKEND_SERVER_URI}/api/auth/me`, {
-            headers: { Authorization: `Bearer ${token}` },
-          });
-          const userData = res.data;
+  const LinkedInLogin = async () => {
+    window.location.href = `${process.env.REACT_APP_BACKEND_SERVER_URI}/api/auth/linkedin`;
+  };
 
-        
-          localStorage.setItem('user', JSON.stringify(userData));
-          localStorage.setItem('email',JSON.stringify(userData.email) );
-          dispatch(setUser(userData));
+  const handleLinkedInResponse = useCallback(async () => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get('token');
 
-       
-          navigate('/');
-        } catch (error) {
-          console.error('Error fetching user details:', error);
-        }
+    if (token) {
+      // Save token to localStorage
+      localStorage.setItem('token', token);
+
+      try {
+        const res = await axios.get(`${process.env.REACT_APP_BACKEND_SERVER_URI}/api/auth/me`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        const userData = res.data;
+
+        localStorage.setItem('user', JSON.stringify(userData));
+        localStorage.setItem('email', JSON.stringify(userData.email));
+        dispatch(setUser(userData));
+
+        navigate('/');
+      } catch (error) {
+        console.error('Error fetching user details:', error);
       }
-    };
-
-    handleLinkedInResponse();
+    }
   }, [dispatch, navigate]);
+
+  useEffect(() => {
+    handleLinkedInResponse();
+  }, [handleLinkedInResponse]);
+
   return (
     <section className="account-section mt-5">
       <div className="tf-container">
         <div className="row">
-             {error && (
-              <div className="error-container">
+          {error && (
+            <div className="error-container">
               <p>{error}</p>
               <button className="close-btn" onClick={() => setError(null)}>✖</button>
-              </div>
-              )}
+            </div>
+          )}
           <div className="wd-form-login">
             <h4>Log In</h4>
             <form action="/">
@@ -107,10 +107,8 @@ function SignIn() {
                     onChange={(e) => setPassword(e.target.value)}
                   />
 
-                  <Link
-                    className={`password-addon ${
-                      showPass ? "icon-eye" : "icon-eye-off"
-                    }`}
+                  <button
+                    className={`password-addon ${showPass ? "icon-eye" : "icon-eye-off"}`}
                     id="password-addon"
                     onClick={() => setShowPass(!showPass)}
                   />
@@ -122,32 +120,32 @@ function SignIn() {
                   Remember me
                 </div>
                 <Link to="#" className="forgot">
-                  Fogot password?
+                  Forgot password?
                 </Link>
               </div>
-              <button onClick={handleSubmit} style={{marginBottom: '20px'}}> Login</button>
+              <button onClick={handleSubmit} style={{ marginBottom: '20px' }}> Login</button>
               <p className="line-ip">
                 <span>or sign up with</span>
               </p>
 
-              <button 
-            onClick={LinkedInLogin} 
-            style={{
-                display: 'flex', 
-                alignItems: 'center', 
-                backgroundColor: '#0077B5',e
-                color: 'white', 
-                border: 'none', 
-                borderRadius: '5px', 
-                padding: '10px 20px', 
-                cursor: 'pointer',
-                fontSize: '16px',
-            }}
-        >
-            <FaLinkedin style={{ marginRight: '8px' }} />
-            Continue with LinkedIn
-        </button>
-           
+              <button
+                onClick={LinkedInLogin}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  backgroundColor: '#0077B5',
+                  color: 'white',
+                  border: 'none',
+                  borderRadius: '5px',
+                  padding: '10px 20px',
+                  cursor: 'pointer',
+                  fontSize: '16px',
+                }}
+              >
+                <FaLinkedin style={{ marginRight: '8px' }} />
+                Continue with LinkedIn
+              </button>
+
               <div className="sign-up">
                 Not registered yet? <Link to="/createaccount">Sign Up</Link>
               </div>
